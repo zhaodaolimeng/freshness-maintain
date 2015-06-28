ff18e96321bdbef3634e493928878008f7c21b2d
zhaodaolimeng@gmail.com <zhaodaolimeng@gmail.com>
2015-06-28 11:09:12 +0800

    quick save

--- matlab/Test4TimeRange.m
+++ matlab/Test4TimeRange.m
@@ -1,12 +1,12 @@
-% Benckmark of EasiCrawl with real events instead of LatencyExpectation
+% Expectation under different timeRange
 
 clear;clc;
 
 sensors = 10;
 lambdaList = rand(1,sensors)*0.9+0.1;
-maxcs = 10; mincs = 2; % crawl per each sensor
-maxwp = 10; minwp = 3; % count of working cycle 
-maxwr = 8; minwr = 2; % length of working range
+maxcs = 20; mincs = 10; % crawl per each sensor
+maxwp = 5; minwp = 2; % count of working cycle 
+maxwr = 4; minwr = 1; % length of working range
 crawlLimitList = randi([mincs,maxcs],1,sensors);
 % timeRange = 500;
 % timeTable = MakeTimeTable(sensors,maxwp,minwp,maxwr,minwr,timeRange);
@@ -18,7 +18,7 @@ sensorType = ones(1,sensors);
 
 % Time Range
 timeRangeList = 1:20;
-timeRangeStep = 200;
+timeRangeStep = 50;
 discreteStep = 2;
 % [opt,arrange] = EasiCrawl(lambdaList,timeTable,crawlLimitList,sumOfCrawl,discreteStep,eps,iteratorLimit);
 
@@ -29,7 +29,7 @@ timeList = zeros(size(timeRangeList));
 
 for i = timeRangeList
     timeRange = timeRangeStep * i;
-    timeTable = MakeTimeTable(sensors,maxwp,minwp,maxwr,minwr,timeRange);
+    timeTable = MakeTimeTable(sensors,i*maxwp,i*minwp,i*maxwr,i*minwr,timeRange);
     startTime = cputime;
     [opt] = EasiCrawl(lambdaList,timeTable,crawlLimitList,sumOfCrawl,discreteStep,eps,iteratorLimit,sensorWeight,sensorType);
     duration = cputime - startTime;
